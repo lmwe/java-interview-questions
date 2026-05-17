7e440f3759647f42a04f5ad73856d8a92c539fd0
Java Interview Questions <java-interview@example.com>
2026-05-17 12:20:36 +0000

    Fix blank page issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Category from "@/pages/Category";
 import QuestionDetail from "@/pages/QuestionDetail";
